bb14ff07552ca71d91862497c079f3baa2c502ed
Sergio Arroutbi Braojos <sergio.arroutbi.braojos@ericsson.com>
2018-03-22 15:36:48 +0100

    Adding a case with Exception Throwing

--- Boost.Test/test/CalcTest.cpp
+++ Boost.Test/test/CalcTest.cpp
@@ -9,3 +9,10 @@ BOOST_AUTO_TEST_CASE( GivenCalculator_WhenAdd0_0_ThenResultIs_0 )
   Calculator calc;
   BOOST_CHECK_EQUAL(0, calc.Add(0,0));
 }
+
+BOOST_AUTO_TEST_CASE( GivenCalculator_WhenDivide1_0_ThenExceptionIsThrown )
+{
+  Calculator calc;
+  BOOST_CHECK_THROW(calc.Divide(1,0), std::invalid_argument);
+
+}
